42a3525f6cc70f68b434c105f93dfc1430f386bc
Aaron Montoya <aatoya7@gmail.com>
2018-02-02 12:07:18 -0700

    Setup for OOP Array assignment

---
+++ DataStructures/Model/Structures/Array.hpp
@@ -0,0 +1,17 @@
+//
+//  Array.hpp
+//  DataStructures
+//
+//  Created by Montoya, Aaron on 2/2/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+#include <assert.h>
+#include <iostream>
+
+using namespace std;
+
+#endif /* Array_hpp */
