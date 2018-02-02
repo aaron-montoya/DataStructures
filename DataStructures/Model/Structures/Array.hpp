13859b25c4e75c464dbc375838585e68bfb54a1b
Aaron Montoya <aatoya7@gmail.com>
2018-02-02 12:22:32 -0700

    Almost done with Array.hpp file.

--- DataStructures/Model/Structures/Array.hpp
+++ DataStructures/Model/Structures/Array.hpp
@@ -14,4 +14,95 @@
 
 using namespace std;
 
+template <class Type>
+class Array
+{
+private:
+    Type * internalArray;
+    int size;
+public:
+    //Constructor
+    Array<Type>(int size);
+    
+    //Copy Constructor
+    Array<Type>(const Array<Type> & toCopy);
+    //Destructor
+    ~Array<Type>();
+    
+    //Operators
+    Array<Type> & operator = (const Array<Type> & toReplace);
+    Type& operator [] (int index);
+    Type operator [] (int index) const;
+    
+    //Methods
+    int getSize() const;
+    Type getFromindex(int index);
+    void setAtIndex(int index, Type data);
+};
+
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this->size = size;
+    
+    internalArray = new Type[size];
+}
+
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this->size = toCopy.getSize();
+    
+    //Build Data Structure
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; index++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+}
+
+template <class Type>
+Array<Type> :: ~Array()
+{
+    delete [] internalArray;
+}
+
+template <class Type>
+Array<Type> & Array<Type> :: operator = (const Array<Type> & toAssign)
+{
+    if (&toAssign != this)
+    {
+        if (size != toAssign.getSize())
+        {
+            delete [] internalArray;
+            size = toAssign.getSize();
+            internalArray = new Type [size];
+        }
+        
+        for (int index = 0; index < size; index++)
+        {
+            internalArray[index] = toAssign[index];
+        }
+    }
+    return *this;
+}
+
+template <class Type>
+Type & Array
+
 #endif /* Array_hpp */
+
+
+
+
+
+
+
+
+
+
+
+
+
