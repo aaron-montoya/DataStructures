db03d32d17f7615fc09d1dd6683f82a59d066e73
Aaron Montoya <aatoya7@gmail.com>
2018-02-26 12:21:30 -0700

    Added the DoubleNode.hpp

---
+++ DataStructures/Model/Structures/Linear/DoubleNode.hpp
@@ -0,0 +1,87 @@
+//
+//  DoubleNode.hpp
+//  DataStructures
+//
+//  Created by Montoya, Aaron on 2/26/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef DoubleNode_hpp
+#define DoubleNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class Doublenode : public Node<Type>
+{
+protected:
+    DoubleNode<Type> * previous;
+    DoubleNode<Type> * next;
+public:
+    DoubleNode();
+    DoubleNode(Type data);
+    DoubleNode(Type data, DoubleNode<Type> * previous, DoubleNode<Type> * next);
+    
+    DoubleNode<Type> * getPrevious();
+    DoubleNode<Type> * getNext();
+    
+    void setPrevious(DoubleNode<Type> * previous);
+    void setNext(DoubleNode<Type> * next);
+};
+
+template <class Type>
+DoubleNode<Type> :: DoubleNode() : Node<Type>()
+{
+    previous = nullptr;
+    next = nullptr;
+}
+
+template <class Type>
+DoubleNode<Type> :: DoubleNode(Type data) : Node<Type>(data)
+{
+    previous = nullptr;
+    next = nullptr;
+}
+
+template <class Type>
+DoulbeNode<Type> :: DoubleNode(Type data, DoubleNode<Type> *previous, DoubleNode<Type>* next) : Node<Type>(data)
+{
+    this->previous = previous;
+    this->next = next;
+}
+
+template <class Type>
+void DoubleNode<Type> :: setNext(DoubleNode<Type> * next)
+{
+    this->next = next;
+}
+
+template <class Type>
+void DoubleNode<Type> :: setPrevious(DoubleNode<Type> * previous)
+{
+    this->previous = previous;
+}
+
+template <class Type>
+DoubleNode<Type> * DoubleNode<Type> :: getNext()
+{
+    return next;
+}
+
+#endif /* DoubleNode_hpp */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
