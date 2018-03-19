2cf61d2ecbc54c4a9b28d76d254603e2aca099b3
Aaron Montoya <aatoya7@gmail.com>
2018-03-19 12:45:18 -0600

    Added size, left and right hand operators

--- DataStructures/Model/Structures/NonLinear/Graph.hpp
+++ DataStructures/Model/Structures/NonLinear/Graph.hpp
@@ -55,4 +55,81 @@ public:
     int costTraversal(Graph<Type> & graph, int vertex);
 };
 
+
+template <class Type>
+const int Graph<Type> :: MAXIMUM;
+
+template <class Type>
+Graph<Type> :: Graph()
+{
+    this->vertexCount = 0;
+}
+
+/*
+ Since we cannot "remove" from an array in C++,
+ we only implement adding to a Graph.
+ Java allows for all objects to be set to nuyll but C++
+ does not.
+*/
+
+template <class Type>
+int Graph<Type> :: size() const
+{
+    return vertexCount;
+}
+
+//Left hand side operator
+template <class Type>
+Type& Graph<Type> :: operator[](int vertex)
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+
+//Right hand side operator
+template <class Type>
+Type Graph<Type> :: operator[](int vertex) const
+{
+    assert(vertex < vertexCount);
+    return graphData[vertex];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif /* Graph_hpp */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
