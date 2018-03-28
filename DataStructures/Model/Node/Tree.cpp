86e7c4ee7379d46f4b0516cfe290181057495c9e
Aaron Montoya <aatoya7@gmail.com>
2018-03-28 12:36:02 -0600

    Working on ArtPanel

---
+++ DataStructures/Model/Node/Tree.cpp
@@ -0,0 +1,9 @@
+//
+//  Tree.cpp
+//  DataStructures
+//
+//  Created by Montoya, Aaron on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Tree.hpp"
---
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -0,0 +1,23 @@
+//
+//  BinaryTreeTester.hpp
+//  DataStructures
+//
+//  Created by Montoya, Aaron on 3/27/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeTester_hpp
+#define BinaryTreeTester_hpp
+
+#include "/Users/amon1978/C Plus Plus/DataStructures/DataStructures/Model/Node/BinaryTreeNode.hpp"
+#include <iostream>
+
+class BinaryTreeTester
+{
+private:
+    
+public:
+    
+};
+
+#endif /* BinaryTreeTester_hpp */
