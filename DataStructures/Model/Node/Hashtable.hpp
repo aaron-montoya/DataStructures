1f1946f923ae27155c9bdeb5ce970fa4abc7b17f
Aaron Montoya <aatoya7@gmail.com>
2018-05-03 11:59:36 -0600

    Added resize, handleCollision and findPosition methods to hashtable

--- DataStructures/Model/Node/Hashtable.hpp
+++ DataStructures/Model/Node/Hashtable.hpp
@@ -37,16 +37,123 @@ public:
 template <class Type>
 Hashtable<Type> :: Hashtable()
 {
+    capacity = 101;
+    internalStorage = new HashNode<Type>*[capacity];
+    size = 0;
+    loadFactor = .80;
 }
 
 template <class Type>
 Hashtable<Type> :: ~Hashtable()
 {
+    delete [] internalStorage;
 }
 
 template <class Type>
 bool Hashtable<Type> :: isPrime(long current)
 {
+    return false;
 }
+
+template <class Type>
+long Hashtable<Type> :: findPosition(HashNode<Type> * insert)
+{
+    long insertPosition = insert->getKey() % this->capacity;
+    return insertPosition;
+}
+
+long Hashtable<Type> :: handleCollision(long currentPosition)
+{
+    long shift = 17;
+    
+    for(long position = currentPosition + shift; position != currentPosition; position += shift)
+    {
+        if(position >= capacity)
+        {
+            position = position % capacity;
+        }
+        
+        if(internalStorage[position] == nullptr)
+        {
+            return position;
+        }
+    }
+    
+    return -1;
+}
+
+template <class Type>
+long Hashtable<Type> :: getSize()
+{
+    return this->size;
+}
+
+template <class Type>
+void Hashtable<Type> :: resize()
+{
+    long updatedCapacity = getNextPrime();
+    HashNode<Type> * * tempStorage = new HasNode<Type> * [updatedCapacity];
+    
+    std :: fill_n(tempStorage, updatedCapacity, nullptr);
+    
+    long oldCapacity = this->capacity;
+    this->capacity = updatedCapacity;
+    
+    for(long index = 0; index < oldCapacity; index++)
+    {
+        if(hasTableStore[index] != nullptr)
+        {
+            HashNode<Type> * temp = internalStorage[index];
+            long position = findPosition(temp);
+            if(tempStorage[position] == nullptr)
+            {
+                tempStorage[position] = temp;
+            }
+            else
+            {
+                long updatedPosition = handleCollision(position);
+                if(updatedPosition != -1)
+                {
+                    tempStorage[updatedPosition] = temp;
+                }
+            }
+        }
+    }
+    internalStorage = tempStorage;
+}
+
+template <class Type>
+long Hashtable<Type> :: getNextPrime()
+{
+
+}
+
+template <class Type>
+void Hashtable<Type> :: insert(Type value)
+{
+}
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif /* Hashtable_hpp */
